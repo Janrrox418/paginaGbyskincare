055bbf585ec5cc0b4e0eeb30dd76bb50c25978d2
Janrrox418 <Alejandrohv1120@gmail.com>
2025-08-06 23:20:40 -0400

    Actualizando para moviles

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
-
 function saludo() {
   alert('¡Gracias por visitar GBY Skincare!');
 }
+
+// Cierra automáticamente el menú cuando se hace clic en un enlace
+document.addEventListener("DOMContentLoaded", function () {
+  const offcanvasElement = document.getElementById('sidebar');
+  const offcanvas = bootstrap.Offcanvas.getOrCreateInstance(offcanvasElement);
+
+  const navLinks = offcanvasElement.querySelectorAll('a.nav-link');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      offcanvas.hide();
+    });
+  });
+});
